c60bab3dd6c93d3a2d2508d3aeaa12be957b0a60
DONKO Akowé Raphaël <dar11022054@gmail.com>
2024-01-18 08:52:08 +0000

    Ajout d'une route pour le composant binding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriesComponent } from './categories/categories.component';
 import { NovelleCategorieComponent } from './novelle-categorie/novelle-categorie.component';
+import { BindingComponent } from './binding/binding.component';
 
 const routes: Routes = [
+  {path: "", component: BindingComponent}
   {path: "categories", component: CategoriesComponent},
   {path: "nouvelle-categorie", component: NovelleCategorieComponent}
 ];
